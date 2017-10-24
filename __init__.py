8b8a0cc47ec14da49fd4e8e9ef91481ad5535339
Jiajie Yan <svenyan234@gmail.com>
2017-10-24 01:26:27 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from Naive_Bayes_Model import NaiveBayes
+from Document import Document
+
+def process_data(source):
+    data = []
+    '''
+    Method to process source data to make them Document instances.
+    '''
+    return data
+
+source = '' # load data from here
+train_set = process_data(source)
+nb = NaiveBayes()
+nb.train(train_set)
